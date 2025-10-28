34ece6136524d389985d7d1be342ccbbbf1f95af
rocketads-tech <rocketads.tech@yandex.ru>
2025-10-28 10:22:09 +0300

    upgraide files, styles, scripts

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+//* ======== check webp start ==========
+function testWebP(callback) {
+  let webP = new Image();
+  webP.onload = function () {
+    callback(webP.complete && webP.height === 2);
+  };
+  webP.onerror = function () {
+    callback(false);
+  };
+  webP.src =
+    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+// Добавляем класс _webp или _no-webp в HTML
+document.addEventListener("DOMContentLoaded", function () {
+  testWebP(function (support) {
+    document.documentElement.classList.add(support ? "webp" : "no-webp");
+  });
+});
+//* ========  check webp end ===========
+
 document.addEventListener("DOMContentLoaded", () => {
   // ====== ACCORDION =========
   const accordionItems = document.querySelectorAll(".accordion-item.active");
